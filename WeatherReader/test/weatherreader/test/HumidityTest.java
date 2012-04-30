7fe2dbfe0617dbf7c46f93d991c3703da04d91c4
paulchen <paulchen@rueckgr.at>
2012-04-30 22:02:32 +0000

    update
    git-svn-id: https://svn.rueckgr.at/private/diplomarbeit@1939 95840bc9-f7e0-4b9f-abbd-54bb5795df2f

--- WeatherReader/test/weatherreader/test/HumidityTest.java
+++ WeatherReader/test/weatherreader/test/HumidityTest.java
@@ -9,7 +9,7 @@ import weatherreader.model.WeatherReport;
 import weatherreader.test.base.IndividualsTest;
 
 import com.hp.hpl.jena.ontology.Individual;
-import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 // TODO javadoc
@@ -18,10 +18,13 @@ public class HumidityTest extends IndividualsTest {
 		String[] concepts = { "Humidity", "VeryDry", "Dry", "NormalHumidity", "Moist", "VeryMoist" };
 		List<String> expected = Arrays.asList(expectedConcepts);
 		
-		Individual weatherPhenomenon = createSingleWeatherPhenomenon();
-		Statement statement = getOnto().createLiteralStatement(weatherPhenomenon, getOnto().getProperty(WeatherReport.NAMESPACE + "hasHumidityValue"), humidityValue);
+		Resource blankNode = getOnto().createResource();
+		getOnto().add(getOnto().createLiteralStatement(blankNode, getOnto().getProperty(WeatherReport.MUO_NAMESPACE + "numericalValue"), humidityValue));
+		// TODO get rid of magic constant for individual name here
+		getOnto().add(getOnto().createStatement(blankNode, getOnto().getProperty(WeatherReport.MUO_NAMESPACE + "measuredIn"), getOnto().getResource("http://purl.oclc.org/NET/muo/ucum/unit/fraction/percent")));
 		
-		getOnto().add(statement);
+		Individual weatherPhenomenon = createSingleWeatherPhenomenon();
+		getOnto().add(getOnto().createStatement(weatherPhenomenon, getOnto().getProperty(WeatherReport.NAMESPACE + "hasHumidityValue"), blankNode));
 		
 		for(String concept : concepts) {
 			assertEquals(expected.contains(concept) ? 1 : 0, getOnto().listStatements(weatherPhenomenon, RDF.type, getOnto().getOntClass(WeatherReport.NAMESPACE + concept)).toSet().size());
