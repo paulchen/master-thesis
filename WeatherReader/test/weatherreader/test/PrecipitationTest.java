46721c7961dd426b0658429ad1a1e07f11739e47
paulchen <paulchen@rueckgr.at>
2012-05-01 16:57:40 +0000

    update
    git-svn-id: https://svn.rueckgr.at/private/diplomarbeit@1948 95840bc9-f7e0-4b9f-abbd-54bb5795df2f

--- WeatherReader/test/weatherreader/test/PrecipitationTest.java
+++ WeatherReader/test/weatherreader/test/PrecipitationTest.java
@@ -9,7 +9,7 @@ import weatherreader.model.WeatherReport;
 import weatherreader.test.base.IndividualsTest;
 
 import com.hp.hpl.jena.ontology.Individual;
-import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 // TODO javadoc
@@ -21,11 +21,18 @@ public class PrecipitationTest extends IndividualsTest {
 		Float floatProbability = (float)precipitationProbability/100;
 		
 		Individual weatherPhenomenon = createSingleWeatherPhenomenon();
-		Statement statement1 = getOnto().createLiteralStatement(weatherPhenomenon, getOnto().getProperty(WeatherReport.NAMESPACE + "hasPrecipitationIntensity"), precipitationIntensity);
-		Statement statement2 = getOnto().createLiteralStatement(weatherPhenomenon, getOnto().getProperty(WeatherReport.NAMESPACE + "hasPrecipitationProbability"), floatProbability);
 		
-		getOnto().add(statement1);
-		getOnto().add(statement2);
+		Resource blankNode1 = getOnto().createResource();
+		getOnto().add(getOnto().createLiteralStatement(blankNode1, getOnto().getProperty(WeatherReport.MUO_NAMESPACE + "numericalValue"), floatProbability));
+		// TODO get rid of magic constant for individual name here
+		getOnto().add(getOnto().createStatement(blankNode1, getOnto().getProperty(WeatherReport.MUO_NAMESPACE + "measuredIn"), getOnto().getResource("http://purl.oclc.org/NET/muo/ucum/unit/fraction/percent")));
+		
+		Resource blankNode2 = getOnto().createResource();
+		getOnto().add(getOnto().createLiteralStatement(blankNode2, getOnto().getProperty(WeatherReport.MUO_NAMESPACE + "numericalValue"), precipitationIntensity));
+		getOnto().add(getOnto().createStatement(blankNode2, getOnto().getProperty(WeatherReport.MUO_NAMESPACE + "measuredIn"), getOnto().getResource(WeatherReport.NAMESPACE + "millimetresPerHour")));
+		
+		getOnto().add(getOnto().createStatement(weatherPhenomenon, getOnto().getProperty(WeatherReport.NAMESPACE + "hasPrecipitationProbability"), blankNode1));
+		getOnto().add(getOnto().createStatement(weatherPhenomenon, getOnto().getProperty(WeatherReport.NAMESPACE + "hasPrecipitationIntensity"), blankNode2));
 		
 		for(String concept : concepts) {
 			assertEquals(expected.contains(concept) ? 1 : 0, getOnto().listStatements(weatherPhenomenon, RDF.type, getOnto().getOntClass(WeatherReport.NAMESPACE + concept)).toSet().size());
