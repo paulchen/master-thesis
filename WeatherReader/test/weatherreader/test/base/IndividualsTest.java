b74b14be3b7cf9f1ae4e02152dec2782983d72e3
paulchen <paulchen@rueckgr.at>
2012-05-26 20:14:04 +0000

    cleanup
    git-svn-id: https://svn.rueckgr.at/private/diplomarbeit@2018 95840bc9-f7e0-4b9f-abbd-54bb5795df2f

--- WeatherReader/test/weatherreader/test/base/IndividualsTest.java
+++ WeatherReader/test/weatherreader/test/base/IndividualsTest.java
@@ -1,9 +1,5 @@
 package weatherreader.test.base;
 
-import java.math.BigDecimal;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
 import junit.framework.TestCase;
 
 import org.junit.After;
@@ -12,13 +8,11 @@ import org.mindswap.pellet.jena.PelletReasonerFactory;
 
 import weatherreader.model.WeatherReport;
 
-import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.RDFReader;
-import com.hp.hpl.jena.rdf.model.Resource;
 
 // TODO javadoc
 public abstract class IndividualsTest extends TestCase {
@@ -36,36 +30,6 @@ public abstract class IndividualsTest extends TestCase {
 		RDFReader arp = onto.getReader("RDF/XML");
 		arp.setProperty("embedding", "true");
 		arp.read(onto, "file:ThinkHomeWeather.owl");
-		
-		// TODO clean up here?
-		Resource instantClass = onto.getResource(WeatherReport.TIME + "Instant");
-		Individual instant = onto.createIndividual(WeatherReport.NAMESPACE + "instant0", instantClass);
-
-		Calendar calendar = new GregorianCalendar();
-		Resource dateTimeClass = onto.getResource(WeatherReport.TIME + "DateTimeDescription");
-		Individual dateTime = onto.createIndividual(WeatherReport.NAMESPACE + "dateTime0", dateTimeClass);
-		
-		onto.add(onto.createStatement(dateTime, onto.getProperty(WeatherReport.TIME + "unitType"), onto.getResource(WeatherReport.TIME + "unitMinute")));
-		onto.add(onto.createLiteralStatement(dateTime, onto.getProperty(WeatherReport.TIME + "minute"), new BigDecimal(calendar.get(Calendar.MINUTE))));
-		onto.add(onto.createLiteralStatement(dateTime, onto.getProperty(WeatherReport.TIME + "hour"), new BigDecimal(calendar.get(Calendar.HOUR_OF_DAY))));
-		
-		String dayString = "---";
-		if(calendar.get(Calendar.DAY_OF_MONTH) < 10) {
-			dayString += "0";
-		}
-		dayString += calendar.get(Calendar.DAY_OF_MONTH);
-		onto.add(onto.createStatement(dateTime, onto.getProperty(WeatherReport.TIME + "day"), onto.createTypedLiteral(dayString, XSDDatatype.XSDgDay)));
-		
-		String monthString = "--";
-		if(calendar.get(Calendar.MONTH) < 10) {
-			monthString += "0";
-		}
-		monthString += calendar.get(Calendar.MONTH);
-		onto.add(onto.createStatement(dateTime, onto.getProperty(WeatherReport.TIME + "month"), onto.createTypedLiteral(monthString, XSDDatatype.XSDgMonth)));
-		
-		onto.add(onto.createStatement(dateTime, onto.getProperty(WeatherReport.TIME + "year"), onto.createTypedLiteral(String.valueOf(calendar.get(Calendar.YEAR)), XSDDatatype.XSDgYear)));
-		
-		onto.add(onto.createStatement(instant, onto.getProperty(WeatherReport.TIME + "inDateTime"), dateTime));
 	}
 	
 	@After
