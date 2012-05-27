16c7632d4f8bdbd5c8e6fe88d7175e19a6b6f168
paulchen <paulchen@rueckgr.at>
2012-05-27 12:55:51 +0000

    improved junit tests
    git-svn-id: https://svn.rueckgr.at/private/diplomarbeit@2032 95840bc9-f7e0-4b9f-abbd-54bb5795df2f

--- WeatherReader/test/weatherreader/test/WeatherStateTest.java
+++ WeatherReader/test/weatherreader/test/WeatherStateTest.java
@@ -1,5 +1,8 @@
 package weatherreader.test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 import org.junit.After;
@@ -8,13 +11,13 @@ import org.junit.Test;
 import org.mindswap.pellet.jena.PelletReasonerFactory;
 
 import weatherreader.model.WeatherReport;
+import weatherreader.test.model.WeatherPhenomenon;
+import weatherreader.test.model.WeatherState;
+import weatherreader.test.model.Wind;
 
-import com.hp.hpl.jena.ontology.Individual;
-import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.RDFReader;
-import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 public class WeatherStateTest extends TestCase {
@@ -31,28 +34,40 @@ public class WeatherStateTest extends TestCase {
 	
 	@After
 	public void tearDown() {
+		/* nothing to do */
+	}
+	
+	private void testState(WeatherPhenomenon[] phenomena, String[] expectedConcepts) {
+		String[] allConcepts = {
+				"CalmWeather", "WindyWeather", "StormyWeather"
+			};
+		List<String> expected = Arrays.asList(expectedConcepts);
+
+		WeatherState state = new WeatherState("weatherState" + stateIndex);
+		stateIndex++;
 		
+		// TODO method addAll?
+		for(WeatherPhenomenon phenomenon : phenomena) {
+			state.addPhenomenon(phenomenon);
+		}
+		
+		state.createIndividuals(onto);
+		
+		for(String concept : allConcepts) {
+			assertEquals(expected.contains(concept) ? 1 : 0, onto.listStatements(state.getOntIndividual(), RDF.type, onto.getOntClass(WeatherReport.NAMESPACE + concept)).toSet().size());
+		}		
 	}
 	
 	@Test
 	public void testWindyWeather() {
-		// TODO duplicate code here?
-		OntClass weatherStateClass = onto.getOntClass(WeatherReport.NAMESPACE + "WeatherState");
-		Individual weatherState = onto.createIndividual(WeatherReport.NAMESPACE + "weatherState" + stateIndex, weatherStateClass);
-		
-		Resource blankNode = onto.createResource();
-		onto.add(onto.createLiteralStatement(blankNode, onto.getProperty(WeatherReport.MUO_NAMESPACE + "numericalValue"), 30f));
-		// TODO get rid of magic constant for individual name here
-		onto.add(onto.createStatement(blankNode, onto.getProperty(WeatherReport.MUO_NAMESPACE + "measuredIn"), onto.getResource(WeatherReport.NAMESPACE + "metresPerSecond")));
-		
-		OntClass weatherPhenomenonClass = onto.getOntClass(WeatherReport.NAMESPACE + "WeatherPhenomenon");
-		Individual weatherPhenomenon = onto.createIndividual(WeatherReport.NAMESPACE + "wind" + stateIndex, weatherPhenomenonClass);
-		onto.add(onto.createStatement(weatherPhenomenon, onto.getProperty(WeatherReport.NAMESPACE + "belongsToWeatherState"), weatherState));
-		onto.add(onto.createStatement(weatherPhenomenon, onto.getProperty(WeatherReport.NAMESPACE + "hasWindSpeed"), blankNode));
-		
-		assertEquals(1, onto.listStatements(weatherPhenomenon, RDF.type, onto.getOntClass(WeatherReport.NAMESPACE + "Storm")).toSet().size());
-		assertEquals(1, onto.listStatements(weatherState, RDF.type, onto.getOntClass(WeatherReport.NAMESPACE + "WindyWeather")).toSet().size());
-		
-		stateIndex++;
+		for(int a=0; a<10; a++) {
+			testState(new WeatherPhenomenon[] { new Wind("wind" + a, a) }, new String[] { "CalmWeather" });
+		}
+		for(int a=10; a<20; a++) {
+			testState(new WeatherPhenomenon[] { new Wind("wind" + a, a) }, new String[] { "WindyWeather" });
+		}
+		for(int a=20; a<100; a++) {
+			testState(new WeatherPhenomenon[] { new Wind("wind" + a, a) }, new String[] { "WindyWeather", "StormyWeather" });
+		}
 	}
 }
