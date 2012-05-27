95e80055de2e2228ce939e1f2f8e0d004a2128f9
paulchen <paulchen@rueckgr.at>
2012-05-27 12:54:27 +0000

    files for improved weather model?
    git-svn-id: https://svn.rueckgr.at/private/diplomarbeit@2030 95840bc9-f7e0-4b9f-abbd-54bb5795df2f

---
+++ WeatherReader/test/weatherreader/test/WeatherPhenomenon.java
@@ -0,0 +1,12 @@
+package weatherreader.test;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntModel;
+
+//TODO javadoc
+public abstract class WeatherPhenomenon {
+
+	public abstract void createIndividuals(OntModel onto);
+
+	protected abstract Individual getOntIndividual();
+}
---
+++ WeatherReader/test/weatherreader/test/Wind.java
@@ -0,0 +1,37 @@
+package weatherreader.test;
+
+import weatherreader.model.WeatherReport;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+// TODO javadoc
+public class Wind extends WeatherPhenomenon {
+	private float windSpeed;
+	private String name;
+	private Individual individual;
+	
+	public Wind(String name, float windSpeed) {
+		this.name = name;
+		this.windSpeed = windSpeed;
+	}
+
+	@Override
+	public void createIndividuals(OntModel onto) {
+		Resource blankNode = onto.createResource();
+		onto.add(onto.createLiteralStatement(blankNode, onto.getProperty(WeatherReport.MUO_NAMESPACE + "numericalValue"), windSpeed));
+		// TODO get rid of magic constant for individual name here
+		onto.add(onto.createStatement(blankNode, onto.getProperty(WeatherReport.MUO_NAMESPACE + "measuredIn"), onto.getResource(WeatherReport.NAMESPACE + "metresPerSecond")));
+		
+		OntClass weatherPhenomenonClass = onto.getOntClass(WeatherReport.NAMESPACE + "WeatherPhenomenon");
+		individual = onto.createIndividual(WeatherReport.NAMESPACE + name, weatherPhenomenonClass);
+		onto.add(onto.createStatement(individual, onto.getProperty(WeatherReport.NAMESPACE + "hasWindSpeed"), blankNode));
+	}
+
+	@Override
+	protected Individual getOntIndividual() {
+		return individual; 
+	}
+}
