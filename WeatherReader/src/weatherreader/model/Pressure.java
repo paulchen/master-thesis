f8bb289e9495de54bad83263e42644056d59cd32
Paul Staroch <paulchen@rueckgr.at>
2012-07-09 23:24:50 +0200

    update due to ontology refactoring

--- WeatherReader/src/weatherreader/model/Pressure.java
+++ WeatherReader/src/weatherreader/model/Pressure.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 public class Pressure extends WeatherPhenomenon {
 	private String name;
@@ -30,10 +31,14 @@ public class Pressure extends WeatherPhenomenon {
 
 	@Override
 	public void createIndividuals(OntModel onto) {
-		OntClass weatherPhenomenonClass = onto.getOntClass(WeatherConstants.NAMESPACE + "WeatherPhenomenon");
-		individual = onto.createIndividual(WeatherConstants.NAMESPACE + name, weatherPhenomenonClass);
+		Resource blankNode = onto.createResource();
+		onto.add(onto.createLiteralStatement(blankNode, onto.getProperty(WeatherConstants.MUO_NAMESPACE + "numericalValue"), roundFloat(pressureValue, WeatherConstants.DECIMALS)));
+		onto.add(onto.createStatement(blankNode, onto.getProperty(WeatherConstants.MUO_NAMESPACE + "measuredIn"), onto.getResource(WeatherConstants.NAMESPACE + "hectopascal")));
 		
-		onto.add(onto.createLiteralStatement(individual, onto.getProperty(WeatherConstants.NAMESPACE + "hasPressureValue"), pressureValue));
+ 		OntClass weatherPhenomenonClass = onto.getOntClass(WeatherConstants.NAMESPACE + "WeatherPhenomenon");
+ 		individual = onto.createIndividual(WeatherConstants.NAMESPACE + name, weatherPhenomenonClass);
+ 		
+		onto.add(onto.createStatement(individual, onto.getProperty(WeatherConstants.NAMESPACE + "hasPressureValue"), blankNode));
 	}
 
 	@Override
