d7497e56469e498d361c6402813a14c652c00b66
Paul Staroch <paulchen@rueckgr.at>
2013-03-29 23:12:13 +0100

    further development

--- WeatherImporter/src/at/ac/tuwien/auto/thinkhome/weatherimporter/model/Humidity.java
+++ WeatherImporter/src/at/ac/tuwien/auto/thinkhome/weatherimporter/model/Humidity.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.auto.thinkhome.weatherimporter.model;
 
 import java.util.List;
 
+import at.ac.tuwien.auto.thinkhome.weatherimporter.turtle.TurtleStatement;
 import at.ac.tuwien.auto.thinkhome.weatherimporter.turtle.TurtleStore;
 
 import com.hp.hpl.jena.ontology.Individual;
@@ -52,7 +53,13 @@ public class Humidity extends WeatherPhenomenon {
 	public TurtleStore getTurtleStatements() {
 		TurtleStore turtle = new TurtleStore();
 		
-		// TODO
+		String blankNode = Weather.generateBlankNode();
+		
+		turtle.add(new TurtleStatement(blankNode, WeatherConstants.MUO_PREFIX + "numericalValue", String.valueOf(roundFloat(humidityValue, WeatherConstants.DECIMALS) + "f")));
+		turtle.add(new TurtleStatement(blankNode, WeatherConstants.MUO_PREFIX + "measuredIn", WeatherConstants.NAMESPACE_PREFIX + "percent"));
+		
+		turtle.add(new TurtleStatement(WeatherConstants.NAMESPACE_PREFIX + name, "a", WeatherConstants.NAMESPACE_PREFIX + "WeatherPhenomenon"));
+		turtle.add(new TurtleStatement(WeatherConstants.NAMESPACE_PREFIX + name, WeatherConstants.NAMESPACE_PREFIX + "hasHumidityValue", blankNode));
 		
 		return turtle;
 	}
