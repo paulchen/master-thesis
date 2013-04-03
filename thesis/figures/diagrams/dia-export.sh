858b3fd657826ba23c8a34113474f4502d86d4b8
Paul Staroch <paulchen@rueckgr.at>
2013-04-03 23:29:41 +0200

    added export script (DIA to PNG)

---
+++ thesis/figures/diagrams/dia-export.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FACTOR=4
+FILES="turtlestore.dia"
+#FILES="*.dia"
+
+for file in $FILES; do
+	dia --export blubb.png $file > /dev/null 2>&1
+	HEIGHT=`identify blubb.png |sed -e "s/^[^x]*x//g;s/ .*//g"`
+	rm blubb.png
+	NEW_HEIGHT=$((HEIGHT*FACTOR))
+	dia --export ${file/dia/png} -s x$NEW_HEIGHT $file
+done
+
