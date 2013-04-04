e77fcc5e7e4dc90eb28b437c242e8eb82aaa9e85
Paul Staroch <paulchen@rueckgr.at>
2013-04-04 23:44:46 +0200

    cleanup

--- WeatherImporter/src/at/ac/tuwien/auto/thinkhome/weatherimporter/model/OntologyClass.java
+++ WeatherImporter/src/at/ac/tuwien/auto/thinkhome/weatherimporter/model/OntologyClass.java
@@ -6,7 +6,6 @@ import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntModel;
 
 // TODO javadoc
-// TODO create TTL file
 public interface OntologyClass {
 	public void createIndividuals(OntModel onto);
 
